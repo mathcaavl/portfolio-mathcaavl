2e62c9c63533022e38c2bcb9c999267ec6ad3939
mathcaavl <mateuscavl1@gmail.com>
2020-09-16 23:40:38 -0300

    Atualizando o meu site

--- Js/script.js
+++ Js/script.js
@@ -1,32 +1,44 @@
-const titulo = document.querySelector(".apresentation-name");
-const repositories = document.querySelector('.repositories-title')
-const name_web_site = document.querySelector('.title-name');
-let contador = 0;
+/* MENU */
+const btnMenu = $('.checkbtn')
+const navList = $('.navigation-menu-list');
 
-$('.card').hover(function() {
-    if (contador == 0) {
-        $(this).css('width', '20em');
-        contador++
-    } else if (contador % 2 == 0) {
-        $(this).css('width', '20em');
-        contador++
+btnMenu.click(function() {
+    if (navList.hasClass('navigation-menu-list-toggle')) {
+        navList.removeClass('navigation-menu-list-toggle')
     } else {
-        $(this).css('transition', '1s')
-        $(this).css('width', '15em');
-        contador++
+        navList.addClass('navigation-menu-list-toggle')
     }
 })
 
-function typeWriter(elemento) {
-    const textoArray = elemento.innerHTML.split("")
-    elemento.innerHTML = ""
-    textoArray.forEach((letra, i) => {
-        setTimeout(function() {
-            elemento.innerHTML += letra
-        }, 80 * i)
-    })
-}
+// const titulo = document.querySelector(".apresentation-name");
+// const repositories = document.querySelector('.repositories-title')
+// const name_web_site = document.querySelector('.title-name');
+// let contador = 0;
 
-typeWriter(titulo);
-typeWriter(repositories)
-typeWriter(name_web_site)
+// $('.card').hover(function() {
+//     if (contador == 0) {
+//         $(this).css('width', '20em');
+//         contador++
+//     } else if (contador % 2 == 0) {
+//         $(this).css('width', '20em');
+//         contador++
+//     } else {
+//         $(this).css('transition', '1s')
+//         $(this).css('width', '15em');
+//         contador++
+//     }
+// })
+
+// function typeWriter(elemento) {
+//     const textoArray = elemento.innerHTML.split("")
+//     elemento.innerHTML = ""
+//     textoArray.forEach((letra, i) => {
+//         setTimeout(function() {
+//             elemento.innerHTML += letra
+//         }, 80 * i)
+//     })
+// }
+
+// typeWriter(titulo);
+// typeWriter(repositories)
+// typeWriter(name_web_site)
